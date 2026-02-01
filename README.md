042552e28211dda3c9e2abce6ea2ae2cdf1a0fec
Orlandis Cuevas <orlandis.cuevas@alten.es>
2026-02-01 19:03:30 +0100

    Paginacion

--- README.md
+++ README.md
@@ -2,17 +2,22 @@
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
 </p>
 
+# Varibales de Desarrollo
+
+`api = localhost:3000/api/v2`
+`api = 127.0.0.1:3000/api/v2`
+
 # Ejecutra en desarrollo
 
 1. Clonar repositorio
 2. Ejecutar
-
    `pnpm install`
-
 3. Tener nest CLI instalado
    `pnpm i -g @nestjs/cli`
 4. Levantar BBDD
    `docker-compose up -d`
+5. Reconstruir BBDD con datos
+   `{{api}}/seed`
 
 ## Stack
 
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
 import { PokeResponse, Result } from './interfaces/poke-response.interface';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
   async executeSeed() {
-    const { data } = await axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=5',
+    await this.pokemonModel.deleteMany();
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=1500',
     );
 
-    data.results.forEach(({ name, url }: Result) => {
-      const segments = url.split('/');
-      const no: number = +segments[segments.length - 2];
-      return { name, no };
+    const pokemons: { name: string; no: number }[] = [];
+
+    data.results.forEach((data) => {
+      const pokemon = this.createPokemon(data);
+      pokemons.push(pokemon);
     });
-    return data.results;
+    const results = await this.pokemonModel.insertMany(pokemons);
+    return { message: 'Seed Executed', count: results.length };
+  }
+  createPokemon(data: Result) {
+    const { name, url } = data;
+    const segments = url.split('/');
+    const no: number = +segments[segments.length - 2];
+
+    return { name, no };
   }
 }
